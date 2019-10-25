8e795005cc59da68640165653bc21748755ba57a
suwalbikesh <suwalbikezh@gmail.com>
2019-10-25 10:49:06 +0545

    Hash Map

--- app/src/main/java/com/bikeshsuwal/listview/MainActivity.java
+++ app/src/main/java/com/bikeshsuwal/listview/MainActivity.java
@@ -6,16 +6,23 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
     private ListView lstcountries;
     public static final String countries[] = {
-      "Nepal",
-      "India",
-      "China",
-      "UK",
-      "USA",
-      "Canada"
+      "Nepal", "Kathmandu",
+      "India", "New Delhi",
+      "China", "Beijing",
+      "UK", "London",
+      "USA", "Washington DC",
+      "Canada", "Ottawa"
     };
+
+    //for hashMap
+    private Map<String, String> countriesMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +31,19 @@ public class MainActivity extends AppCompatActivity {
 
         lstcountries = findViewById(R.id.lstcountries);
 
+        countriesMap = new HashMap<>();
+
+        for (int i = 0; i < countries.length; i += 2){
+            countriesMap.put(countries[i], countries[i + 1]);
+        }
+
         ArrayAdapter arrayAdapter = new ArrayAdapter(
                 this,
                 android.R.layout.simple_list_item_1,
-                countries
+                new ArrayList(countriesMap.keySet())
         );
 
         lstcountries.setAdapter(arrayAdapter);
+
     }
 }
